c56094cc983987d3f719422221467816e3a042e2
Mitsuhiro Kobungyo <kob@localhost.localdomain>
2019-06-30 22:47:50 +0900

    first

---
+++ src/main/java/net/kobungyo/hoge/App.java
@@ -0,0 +1,13 @@
+package net.kobungyo.hoge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
